146da4a8b7a8ed93d6151f4fd4c1dd06b7238c19
RandyLiu6410 <47877698+RandyLiu6410@users.noreply.github.com>
2019-05-26 15:57:43 +0800

    first

--- Assets/ColorBlind/Randy/Script/Block.cs
+++ Assets/ColorBlind/Randy/Script/Block.cs
@@ -4,11 +4,77 @@ using UnityEngine;
 using ZTools;
 
 public class Block : MonoBehaviour {
-    // Start is called before the first frame update
-    void Start () { }
+    private Block[] neightborBlocks = new Block[8];
+    private bool isClick = false;
+    public int colorIndex;
+    
+    //public BlockFeedback blockFeedback;
+    void Start()
+    {
+        
+    }
 
     // Update is called once per frame
-    void Update () {
+    void Update()
+    {
 
     }
+
+    public void SetNeightborBlocks(int index, Block block)
+    {
+        neightborBlocks[index] = block;
+    }
+    public Block[] GetNeightborBlocks()
+    {
+        return neightborBlocks;
+    }
+    public void InitCenter()
+    {
+        // 判斷 neightborBlocks 哪幾個位置沒有方塊
+        // 沒有方塊的 index 就產生方塊存入，並且呼叫 InitColor()
+        // 並且把對應的方塊指給每一個 neightborBlock
+        // 更新 MapManager 的 currCenterBlock 為自己
+        // 更新 MapManager 的 currColor
+        List<int> null_index = new List<int>();
+        for (int i = 0; i < this.neightborBlocks.Length; i++)
+        {
+            if (this.neightborBlocks[i] == null)
+                null_index.Add(i);
+        }
+
+        foreach (int index in null_index)
+        {
+            var blockIns = GameObject.Instantiate(this);
+            blockIns.transform.position = this.transform.position + MapManager.Instance.neightborVectors[index];
+            blockIns.InitColor();
+            SetNeightborBlocks(index, blockIns);
+        }
+
+        MapManager.Instance.currCenterBlock = this;
+        MapManager.Instance.currColor = this.colorIndex;
+    }
+    public void InitColor()
+    {
+        // 跟 MapManager 取得隨機顏色的位置
+        // 再從 MapManager 取得對應的顏色
+
+        colorIndex = MapManager.Instance.GetRandomColorIndex();
+        this.GetComponent<MeshRenderer>().material.color = MapManager.Instance.GetColorByIndex(colorIndex);
+        Debug.Log(colorIndex + "   " + this.GetComponent<MeshRenderer>().material.color.ToString());
+    }
+    /*
+    private void OnMouseClick()
+    {
+        // unity 內建函數
+        // 跟 MapManager 比對顏色的位置是否ㄧ樣
+        if (same)
+        {
+            InitCenter();
+            blockFeedback.BecomeCenter();
+        }
+        else
+        {
+            blockFeedback.ClickError();
+        }
+    }*/
 }
--- Assets/ColorBlind/Randy/Script/MapManager.cs
+++ Assets/ColorBlind/Randy/Script/MapManager.cs
@@ -3,14 +3,60 @@ using System.Collections.Generic;
 using UnityEngine;
 using ZTools;
 public class MapManager : Singleton<MapManager> {
-    public int t;
     // Start is called before the first frame update
-    void Start () {
+    public string[] colorTextArray = new string[]{"紅","橙","黃","綠","藍","靛","紫","橘",
+                                                "白","黑","灰","粉","棕","發","財","讚"};
+    public Color[] colorArray = new Color[] { new Color(23, 28, 97), new Color(3, 110, 184),
+                                            new Color(0, 162, 154), new Color(0, 162, 154),
+                                            new Color(34, 172, 56), new Color(218, 224, 0),
+                                            new Color(248, 182, 45), new Color(234, 85, 20),
+                                            new Color(195, 13, 35), new Color(62, 58, 57),
+                                            new Color(239, 239, 239)};
+    public Vector3[] neightborVectors; 
+    // 記錄現在正確的顏色
+    public int currColor;
+    public int Combo = 0;
+    public Block currCenterBlock;
+    public string text;
 
+    void Start()
+    {
+        neightborVectors = new Vector3[8] { new Vector3(0,currCenterBlock.transform.localScale.x*1.2f*Mathf.Sqrt(2),0),
+                                            new Vector3(-Mathf.Sqrt(currCenterBlock.transform.localScale.x*1.2f/2),Mathf.Sqrt(currCenterBlock.transform.localScale.x*1.2f/2),0),
+                                            new Vector3(Mathf.Sqrt(currCenterBlock.transform.localScale.x*1.2f/2),Mathf.Sqrt(currCenterBlock.transform.localScale.x*1.2f/2),0),
+                                            new Vector3(-currCenterBlock.transform.localScale.x*1.2f*Mathf.Sqrt(2),0,0),
+                                            new Vector3(currCenterBlock.transform.localScale.x*1.2f*Mathf.Sqrt(2),0,0),
+                                            new Vector3(-Mathf.Sqrt(currCenterBlock.transform.localScale.x*1.2f/2),-Mathf.Sqrt(currCenterBlock.transform.localScale.x*1.2f/2),0),
+                                            new Vector3(Mathf.Sqrt(currCenterBlock.transform.localScale.x*1.2f/2),-Mathf.Sqrt(currCenterBlock.transform.localScale.x*1.2f/2),0),
+                                            new Vector3(0,-currCenterBlock.transform.localScale.x*1.2f*Mathf.Sqrt(2),0)};
+
+        currCenterBlock.InitCenter();
     }
 
     // Update is called once per frame
-    void Update () {
+    void Update()
+    {
+        
+    }
 
+    public int GetRandomColorIndex() {
+        return Random.Range(0, colorArray.Length);
+    }
+    //方塊本身的顏色
+    public Color GetColorByIndex(int index)
+    {
+        return colorArray[index];
+    }
+    public string GetRandomColorString() {
+        int index = Random.Range(0, colorTextArray.Length);
+        return colorTextArray[index];
+    }
+
+    private void GetText()
+    {
+        // 從 currCenterBlock 的相鄰方塊隨機取得 text 的顏色
+        // 並且從 GetRandomColorString() 取得文字
+        currColor = Random.Range(0, currCenterBlock.GetNeightborBlocks().Length);
+        text = GetRandomColorString();
     }
 }
