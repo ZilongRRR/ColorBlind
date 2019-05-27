bca0f7de21211df4713ec0012d9fdbabc1b6af37
Zilong <>
2019-05-27 10:43:36 +0800

    修改顏色顯示錯誤 (要在數字後面加f 不然數字是int會都是0或1) 把射線改道OnMouseDown

--- Assets/ColorBlind/Randy/Script/Block.cs
+++ Assets/ColorBlind/Randy/Script/Block.cs
@@ -12,112 +12,81 @@ public class Block : MonoBehaviour {
 
     public int cooord_x = 0;
     public int cooord_y = 0;
-    
+
     //public BlockFeedback blockFeedback;
-    void Start()
-    {
-        
+    void Start () {
+
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        if (!isClick)
-        {
-            if (Input.GetMouseButtonDown(0))
-            {
-                Ray ray = MapManager.Instance.cam.ScreenPointToRay(Input.mousePosition);
-                RaycastHit hit;
+    void Update () { }
 
-                if (Physics.Raycast(ray, out hit))
-                {
-                    if (this.gameObject == hit.collider.gameObject)
-                    {
-                        for (int i = 0; i < 9; i++)
-                        {
-                            if (MapManager.Instance.currCenterBlock.neightborBlocks[i] == this)
-                            {
-                                Color color = gameObject.GetComponent<MeshRenderer>().material.color;
-                                color.a = 0f;
-                                gameObject.GetComponent<MeshRenderer>().material.color = color;
-                                MapManager.Instance.clicked_blocks.Add(new int[] { cooord_x, cooord_y });
-
-                                InitCenter();
-                                this.isClick = true;
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    public void SetNeightborBlocks(int index, Block block)
-    {
+    public void SetNeightborBlocks (int index, Block block) {
         neightborBlocks[index] = block;
     }
-    public Block[] GetNeightborBlocks()
-    {
+    public Block[] GetNeightborBlocks () {
         return neightborBlocks;
     }
-    public void InitCenter()
-    {
+    public void InitCenter () {
         // 判斷 neightborBlocks 哪幾個位置沒有方塊
         // 沒有方塊的 index 就產生方塊存入，並且呼叫 InitColor()
         // 並且把對應的方塊指給每一個 neightborBlock
         // 更新 MapManager 的 currCenterBlock 為自己
         // 更新 MapManager 的 currColor
-        List<int> null_index = new List<int>();
-        GameObject[] _Blocks = GameObject.FindGameObjectsWithTag("Block");
-        foreach(KeyValuePair<int,int[]> item in MapManager.Instance.location_coord)
-        {
+        List<int> null_index = new List<int> ();
+        GameObject[] _Blocks = GameObject.FindGameObjectsWithTag ("Block");
+        foreach (KeyValuePair<int, int[]> item in MapManager.Instance.location_coord) {
             bool check = false;
-            foreach (GameObject go in _Blocks)
-            {
-                if (go.GetComponent<Block>().cooord_x == this.cooord_x + item.Value[0] && go.GetComponent<Block>().cooord_y == this.cooord_y + item.Value[1])
-                {
+            foreach (GameObject go in _Blocks) {
+                if (go.GetComponent<Block> ().cooord_x == this.cooord_x + item.Value[0] && go.GetComponent<Block> ().cooord_y == this.cooord_y + item.Value[1]) {
                     check = true;
-                    this.neightborBlocks[item.Key] = go.GetComponent<Block>();
+                    this.neightborBlocks[item.Key] = go.GetComponent<Block> ();
                 }
             }
             if (item.Key != 4 && !check)
-                null_index.Add(item.Key);
+                null_index.Add (item.Key);
         }
 
         string s = "";
         foreach (int i in null_index) s += i + " ";
         //Debug.Log(s);
 
-        foreach (int index in null_index)
-        {
-            var blockIns = GameObject.Instantiate(this);
-            blockIns.gameObject.SetActive(true);
+        foreach (int index in null_index) {
+            var blockIns = GameObject.Instantiate (this);
+            blockIns.gameObject.SetActive (true);
             blockIns.transform.position = this.transform.position + MapManager.Instance.neightborVectors[index];
             blockIns.cooord_x = this.cooord_x + MapManager.Instance.location_coord[index][0];
             blockIns.cooord_y = this.cooord_y + MapManager.Instance.location_coord[index][1];
-            blockIns.InitColor();
-            SetNeightborBlocks(index, blockIns);
+            blockIns.InitColor ();
+            SetNeightborBlocks (index, blockIns);
         }
 
         MapManager.Instance.currCenterBlock = this;
         MapManager.Instance.currColor = this.colorIndex;
     }
-    public void InitColor()
-    {
+    public void InitColor () {
         // 跟 MapManager 取得隨機顏色的位置
         // 再從 MapManager 取得對應的顏色
 
-        colorIndex = MapManager.Instance.GetRandomColorIndex();
-        this.GetComponent<MeshRenderer>().material.color = MapManager.Instance.GetColorByIndex(colorIndex);
+        colorIndex = MapManager.Instance.GetRandomColorIndex ();
+        this.GetComponent<MeshRenderer> ().material.color = MapManager.Instance.GetColorByIndex (colorIndex);
     }
-    
-    private void OnMouseClick()
-    {
+    private void OnMouseDown () {
         // unity 內建函數
         // 跟 MapManager 比對顏色的位置是否ㄧ樣
-        this.GetComponent<MeshRenderer>().material.color = Color.red;
-        this.InitCenter();
+        if (!isClick) {
+            for (int i = 0; i < 9; i++) {
+                if (MapManager.Instance.currCenterBlock.neightborBlocks[i] == this) {
+                    Color color = gameObject.GetComponent<MeshRenderer> ().material.color;
+                    color.a = 0f;
+                    gameObject.GetComponent<MeshRenderer> ().material.color = color;
+                    MapManager.Instance.clicked_blocks.Add (new int[] { cooord_x, cooord_y });
 
+                    InitCenter ();
+                    this.isClick = true;
+                }
+            }
+        }
         /*if (same)
         {
             InitCenter();
@@ -129,14 +98,13 @@ public class Block : MonoBehaviour {
         }*/
     }
 
-    public static void SetMaterialRenderingModeTransparent(Material material)
-    {
-        material.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.One);
-        material.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
-        material.SetInt("_ZWrite", 0);
-        material.DisableKeyword("_ALPHATEST_ON");
-        material.DisableKeyword("_ALPHABLEND_ON");
-        material.EnableKeyword("_ALPHAPREMULTIPLY_ON");
+    public static void SetMaterialRenderingModeTransparent (Material material) {
+        material.SetInt ("_SrcBlend", (int) UnityEngine.Rendering.BlendMode.One);
+        material.SetInt ("_DstBlend", (int) UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
+        material.SetInt ("_ZWrite", 0);
+        material.DisableKeyword ("_ALPHATEST_ON");
+        material.DisableKeyword ("_ALPHABLEND_ON");
+        material.EnableKeyword ("_ALPHAPREMULTIPLY_ON");
         material.renderQueue = 3000;
     }
 }
--- Assets/ColorBlind/Randy/Script/MapManager.cs
+++ Assets/ColorBlind/Randy/Script/MapManager.cs
@@ -1,24 +1,49 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using ZTools;
 using UnityEngine.UI;
+using ZTools;
 public class MapManager : Singleton<MapManager> {
     // Start is called before the first frame update
     [SerializeField] public Camera cam;
-    public string[] colorTextArray = new string[]{"紅","橙","黃","綠","藍","靛","紫","橘",
-                                                "白","黑","灰","粉","棕","發","財","讚"};
-    public Color[] colorArray = new Color[] { new Color(23/255, 28/255, 97/255), new Color(3/255, 110/255, 184/255),
-                                            new Color(0, 162/255, 154/255), new Color(0, 162/255, 154/255),
-                                            new Color(34/255, 172/255, 56/255), new Color(218/255, 224/255, 0),
-                                            new Color(248/255, 182/255, 45/255), new Color(234/255, 85/255, 20/255),
-                                            new Color(195/255, 13/255, 35/255), new Color(62/255, 58/255, 57/255),
-                                            new Color(239/255, 239/255, 239/255)};
+    public string[] colorTextArray = new string[] {
+        "紅",
+        "橙",
+        "黃",
+        "綠",
+        "藍",
+        "靛",
+        "紫",
+        "橘",
+        "白",
+        "黑",
+        "灰",
+        "粉",
+        "棕",
+        "發",
+        "財",
+        "讚"
+    };
+    public Color[] colorArray = new Color[] {
+        new Color (23f / 255f, 28f / 255f, 97f / 255f), new Color (3f / 255f, 110f / 255f, 184f / 255f),
+        new Color (0f, 162f / 255f, 154f / 255f), new Color (0f, 162f / 255f, 154f / 255f),
+        new Color (34f / 255f, 172f / 255f, 56f / 255f), new Color (218f / 255f, 224f / 255f, 0f),
+        new Color (248f / 255f, 182f / 255f, 45f / 255f), new Color (234f / 255f, 85f / 255f, 20f / 255f),
+        new Color (195f / 255f, 13f / 255f, 35f / 255f), new Color (62f / 255f, 58f / 255f, 57f / 255f),
+        new Color (239f / 255f, 239f / 255f, 239f / 255f)
+    };
     public Vector3[] neightborVectors;
-    public Dictionary<int, int[]> location_coord = new Dictionary<int, int[]> { { 0, new int[]{-1, 1 } }, { 1, new int[] { 0, 1 } } ,{ 2, new int[] { 1, 1 } } ,
-                                                                                { 3, new int[] { -1, 0 } } ,{ 4, new int[] { 0, 0 } },{ 5, new int[] { 1, 0 } },
-                                                                                { 6, new int[] { -1, -1 } },{ 7, new int[] { 0, -1 } },{ 8, new int[] { 1, -1 } }};
-    public List<int[]> clicked_blocks = new List<int[]>();
+    public Dictionary<int, int[]> location_coord = new Dictionary<int, int[]> { { 0, new int[] {-1, 1 } },
+        { 1, new int[] { 0, 1 } },
+        { 2, new int[] { 1, 1 } },
+        { 3, new int[] {-1, 0 } },
+        { 4, new int[] { 0, 0 } },
+        { 5, new int[] { 1, 0 } },
+        { 6, new int[] {-1, -1 } },
+        { 7, new int[] { 0, -1 } },
+        { 8, new int[] { 1, -1 } }
+    };
+    public List<int[]> clicked_blocks = new List<int[]> ();
     // 記錄現在正確的顏色
     public int currColor;
     public int Combo = 0;
@@ -26,76 +51,71 @@ public class MapManager : Singleton<MapManager> {
     public Text text;
     private float distance = 1.1f;
 
-    void Start()
-    {
-        neightborVectors = new Vector3[9] { new Vector3(0,currCenterBlock.transform.localScale.x*distance*Mathf.Sqrt(2),0),
-                                            new Vector3(currCenterBlock.transform.localScale.x*distance/Mathf.Sqrt(2),currCenterBlock.transform.localScale.x*distance/Mathf.Sqrt(2),0),
-                                            new Vector3(currCenterBlock.transform.localScale.x*distance*Mathf.Sqrt(2),0,0),
-                                            new Vector3(-currCenterBlock.transform.localScale.x*distance/Mathf.Sqrt(2),currCenterBlock.transform.localScale.x*distance/Mathf.Sqrt(2),0),
-                                            new Vector3(0,0,0),
-                                            new Vector3(currCenterBlock.transform.localScale.x*distance/Mathf.Sqrt(2),-currCenterBlock.transform.localScale.x*distance/Mathf.Sqrt(2),0),
-                                            new Vector3(-currCenterBlock.transform.localScale.x*distance*Mathf.Sqrt(2),0,0),
-                                            new Vector3(-currCenterBlock.transform.localScale.x*distance/Mathf.Sqrt(2),-currCenterBlock.transform.localScale.x*distance/Mathf.Sqrt(2),0),
-                                            new Vector3(0,-currCenterBlock.transform.localScale.x*distance*Mathf.Sqrt(2),0)};
-        
-        for(int index = 0; index < 9; index++)
-        {
-            var blockIns = GameObject.Instantiate(currCenterBlock);
+    void Start () {
+        neightborVectors = new Vector3[9] {
+            new Vector3 (0, currCenterBlock.transform.localScale.x * distance * Mathf.Sqrt (2), 0),
+            new Vector3 (currCenterBlock.transform.localScale.x * distance / Mathf.Sqrt (2), currCenterBlock.transform.localScale.x * distance / Mathf.Sqrt (2), 0),
+            new Vector3 (currCenterBlock.transform.localScale.x * distance * Mathf.Sqrt (2), 0, 0),
+            new Vector3 (-currCenterBlock.transform.localScale.x * distance / Mathf.Sqrt (2), currCenterBlock.transform.localScale.x * distance / Mathf.Sqrt (2), 0),
+            new Vector3 (0, 0, 0),
+            new Vector3 (currCenterBlock.transform.localScale.x * distance / Mathf.Sqrt (2), -currCenterBlock.transform.localScale.x * distance / Mathf.Sqrt (2), 0),
+            new Vector3 (-currCenterBlock.transform.localScale.x * distance * Mathf.Sqrt (2), 0, 0),
+            new Vector3 (-currCenterBlock.transform.localScale.x * distance / Mathf.Sqrt (2), -currCenterBlock.transform.localScale.x * distance / Mathf.Sqrt (2), 0),
+            new Vector3 (0, -currCenterBlock.transform.localScale.x * distance * Mathf.Sqrt (2), 0)
+        };
+        for (int index = 0; index < 9; index++) {
+            var blockIns = GameObject.Instantiate (currCenterBlock);
             blockIns.transform.position = currCenterBlock.transform.position + neightborVectors[index];
             blockIns.cooord_x = currCenterBlock.cooord_x + location_coord[index][0];
             blockIns.cooord_y = currCenterBlock.cooord_y + location_coord[index][1];
-            blockIns.InitColor();
-            currCenterBlock.SetNeightborBlocks(index, blockIns);
+            blockIns.InitColor ();
+            currCenterBlock.SetNeightborBlocks (index, blockIns);
         }
-        currCenterBlock.InitCenter();
-        currCenterBlock.gameObject.SetActive(false);
+        currCenterBlock.InitCenter ();
+        currCenterBlock.gameObject.SetActive (false);
 
         //create UI Text
-        GameObject canvasGO = new GameObject();
+        GameObject canvasGO = new GameObject ();
         canvasGO.name = "Canvas";
-        canvasGO.AddComponent<Canvas>();
-        canvasGO.AddComponent<CanvasScaler>();
-        canvasGO.AddComponent<GraphicRaycaster>();
+        canvasGO.AddComponent<Canvas> ();
+        canvasGO.AddComponent<CanvasScaler> ();
+        canvasGO.AddComponent<GraphicRaycaster> ();
 
         // Get canvas from the GameObject.
         Canvas canvas;
-        canvas = canvasGO.GetComponent<Canvas>();
+        canvas = canvasGO.GetComponent<Canvas> ();
         canvas.renderMode = RenderMode.ScreenSpaceOverlay;
 
         // Create the Text GameObject.
-        GameObject textGO = new GameObject();
+        GameObject textGO = new GameObject ();
         textGO.transform.parent = canvasGO.transform;
-        text = textGO.AddComponent<Text>();
+        text = textGO.AddComponent<Text> ();
         text.fontSize = 1;
         text.alignment = TextAnchor.MiddleCenter;
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update () {
+
     }
 
-    public int GetRandomColorIndex() {
-        return Random.Range(0, colorArray.Length);
+    public int GetRandomColorIndex () {
+        return Random.Range (0, colorArray.Length);
     }
     //方塊本身的顏色
-    public Color GetColorByIndex(int index)
-    {
+    public Color GetColorByIndex (int index) {
         return colorArray[index];
     }
-    public string GetRandomColorString() {
-        int index = Random.Range(0, colorTextArray.Length);
+    public string GetRandomColorString () {
+        int index = Random.Range (0, colorTextArray.Length);
         return colorTextArray[index];
     }
-
-    private void GetText()
-    {
+    private void GetText () {
         // 從 currCenterBlock 的相鄰方塊隨機取得 text 的顏色
         // 並且從 GetRandomColorString() 取得文字
-        currColor = Random.Range(0, currCenterBlock.GetNeightborBlocks().Length);
-        text.text = GetRandomColorString();
+        currColor = Random.Range (0, currCenterBlock.GetNeightborBlocks ().Length);
+        text.text = GetRandomColorString ();
         text.transform.position = currCenterBlock.transform.position;
-        text.color = GetColorByIndex(currColor);
+        text.color = GetColorByIndex (currColor);
     }
 }
