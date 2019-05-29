36b6f906ad088688b51c5ce11ebe38d2f1895732
Zilong <>
2019-05-29 19:04:52 +0800

    新增主畫面圖 但還沒有功能

---
+++ Assets/ColorBlind/Z/Script/ColorBlind/ColorData.cs
@@ -0,0 +1,7 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu (menuName = "ScriptableObject Menue/Create ColorData ")]
+public class ColorData : ScriptableObject {
+    public Color[] colorChips;
+}
