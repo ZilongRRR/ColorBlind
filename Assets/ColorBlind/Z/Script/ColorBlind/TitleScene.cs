16a889527ca809f470ce56ecd1b9b5d10e1866bd
Zilong <jim8120004@gmail.com>
2019-06-07 14:55:22 +0800

    修改標題場景

---
+++ Assets/ColorBlind/Z/Script/ColorBlind/TitleScene.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+namespace ZTools {
+    public class TitleScene : MonoBehaviour {
+        public TextMeshProUGUI titleText;
+        public string titleString;
+        public ColorData colorData;
+        int cl;
+        // Start is called before the first frame update
+        void Start () {
+            cl = colorData.colorChips.Length;
+            StartCoroutine (RandomTitleColor ());
+        }
+        IEnumerator RandomTitleColor () {
+            titleText.text = "";
+            var charArray = titleString.ToCharArray ();
+            int ci = Random.Range (0, cl);
+            for (int i = 0; i < charArray.Length; i++) {
+                var hex = ColorUtility.ToHtmlStringRGB (colorData.colorChips[ci]);
+                titleText.text += "<color=#" + hex + ">" + charArray[i].ToString () + "</color>";
+                ci = (ci + 1) % cl;
+            }
+            yield return new WaitForSeconds (2);
+            StartCoroutine (RandomTitleColor ());
+        }
+    }
+}
