238a99ee45a29876a7f61aaa529f5225313db2ae
RandyLiu6410 <wert6410@gmail.com>
2019-06-10 16:10:23 +0800

    ad

---
+++ Assets/ColorBlind/Randy/Script/controlAd.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class controlAd : MonoBehaviour
+{
+    [SerializeField] GameObject content;
+    public GameObject closeAdButton;
+    void Start()
+    {
+        closeAdButton.SetActive(false);
+        this.Invoke("activeButton", 15f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void closeAd()
+    {
+        content.SetActive(false);
+    }
+    void activeButton()
+    {
+        closeAdButton.SetActive(true);
+    }
+}
